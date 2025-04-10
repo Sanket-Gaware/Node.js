07971ac0d7942cdf0b009f5ac1cdfe9e64b9a168
Sanket Gaware <gawaresanket03@gmail.com>
2025-04-10 16:11:25 +0530

    'otp-model-schema'

---
+++ src/Models/Otp.model.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+const OtpSchema = new mongoose.Schema({
+  email: { type: String, required: true },
+  otp: { type: String, required: true },
+  otpExpiry: { type: Date, required: true },
+});
+
+const Otp = mongoose.model("Otp", OtpSchema);
+export default Otp;
